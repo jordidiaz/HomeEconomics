e3542c601d83821527030eee6c59049b7fd743df
Jordi Díaz Añorga <jdiaz@plainconcepts.com>
2019-07-03 21:26:32 +0200

    "Run as a service" capability and deployment script.

---
+++ Deploy/Scripts/RunHomeEconomicsService.bat
@@ -0,0 +1,4 @@
+sc stop HomeEconomics
+sc create HomeEconomics binPath="bin\Debug\netcoreapp2.2\win10-x64\publish\HomeEconomics.exe"
+sc start HomeEconomics
+pause
--- Program.cs
+++ Program.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
 using Microsoft.Extensions.Logging;
 
 namespace HomeEconomics
@@ -14,11 +12,40 @@ namespace HomeEconomics
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var isService = !(Debugger.IsAttached || args.Contains("--console"));
+
+            if (isService)
+            {
+                var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+                var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+                Directory.SetCurrentDirectory(pathToContentRoot);
+            }
+
+            var builder = CreateWebHostBuilder(
+                args.Where(arg => arg != "--console").ToArray());
+
+            var host = builder.Build();
+
+            if (isService)
+            {
+                host.RunAsService();
+            }
+            else
+            {
+                host.Run();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddEventLog();
+                })
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    // Configure the app here.
+                })
                 .UseStartup<Startup>();
     }
 }
