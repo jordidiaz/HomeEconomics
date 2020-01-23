699d039b6aa72f98e61152fa73822b465cb014e9
Jordi Díaz Añorga <jdiaz@plainconcepts.com>
2020-01-23 16:56:49 +0100

    Add allow firewall rule.

--- Deploy/infrastructure.tf
+++ Deploy/infrastructure.tf
@@ -33,6 +33,14 @@ resource "azurerm_sql_server" "sqlServer" {
   administrator_login_password = "${var.sqlServerAdministratorPassword}"
 }
 
+resource "azurerm_sql_firewall_rule" "firewallRule" {
+  name                = "allow"
+  resource_group_name = "${azurerm_resource_group.resourceGroup.name}"
+  server_name         = "${azurerm_sql_server.sqlServer.name}"
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "255.255.255.255"
+}
+
 resource "azurerm_sql_database" "sqlDatabase" {
   name                = "${local.prefix}-sqldatabase"
   resource_group_name = "${azurerm_resource_group.resourceGroup.name}"
