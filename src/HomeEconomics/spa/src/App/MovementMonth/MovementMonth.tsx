191c0f83de149727e7609ddcb35779e278230426
Jordi Diaz Añorga <jordi.diaz@docplanner.com>
2025-07-28 15:21:28 +0200

    Move connectionstring to env var

--- src/HomeEconomics/spa/src/App/MovementMonth/MovementMonth.tsx
+++ src/HomeEconomics/spa/src/App/MovementMonth/MovementMonth.tsx
@@ -108,11 +108,10 @@ const MovementMonth: React.FC<MovementMonthProps> = (props: MovementMonthProps) 
           <form noValidate>
             <select className="select form-control" value={values.year} name="year" onChange={handleChange}>
               <option value="">Año</option>
-              <option value="2020">2020</option>
-              <option value="2021">2021</option>
-              <option value="2022">2022</option>
-              <option value="2023">2023</option>
               <option value="2024">2024</option>
+              <option value="2025">2025</option>
+              <option value="2026">2026</option>
+              <option value="2027">2027</option>
             </select>
             <select className="select form-control" value={values.month} name="month" onChange={handleChange}>
               <option value="">Mes</option>
